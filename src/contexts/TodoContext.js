6acc2f8b3fbf4035808b6f7350ba1cac6a2f2f75
coolnoor19 <nooralam192001@gmail.com>
2024-07-08 10:28:47 +0530

    added new project

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,26 @@
+import{createContext, useContext} from "react"
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo msg",                        //values
+            completed:false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id,todo)=>{},
+    deleteTodo: (id)=>{},                           //method functionality
+    toggleComplete:(id)=>{}   
+})
+
+    
+
+    
+
+export const useTodo =() =>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
+
